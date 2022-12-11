0f4f4d969f05ae58afd4ba024a2bc24d97561901
Blazej Bielski <bbielski@lyck.pl>
2022-12-11 15:57:39 +0100

    basic class country

---
+++ country.py
@@ -0,0 +1,6 @@
+class Country:
+    def __init__(self, capitol, population, square, continent):
+        self.capitol = capitol
+        self.population = population
+        self.square = square
+        self.continent = continent
